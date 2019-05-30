852b4c4d95ee999f5c422eee6cd272b557512117
Rubbedikatz <bonzoxxl@web.de>
2019-05-30 15:20:16 +0200

    Added more inputs to model, cleaned up imports

--- split_model.py
+++ split_model.py
@@ -1,32 +1,34 @@
-from tensorflow.keras.layers import concatenate, LSTM, Dense, Conv1D, Flatten, TimeDistributed, Activation, Input, Dense
+from tensorflow.keras.layers import concatenate, LSTM, Conv1D, Flatten, TimeDistributed, Input, Dense
 from tensorflow.keras.models import Model
 import data_pipeline as dp
 
 # Test dataset compatibility with a CNN + LSTM model layout
 
+window_size = 5
 steps = 1000
 input_dim = 1
-window_size = 5
-
-main_input = Input(shape=(window_size,
-                          steps,
-                          input_dim),
-                   name="Qdlin")
-
-# define CNN model
-cnn_out = TimeDistributed(Conv1D(filters=1, kernel_size=3, activation='relu'))(main_input)
-cnn_flat = TimeDistributed(Flatten())(cnn_out)
-cnn_dense = TimeDistributed(Dense(1))(cnn_flat)
-
-# define LSTM model
-lstm_out = LSTM(50, activation='relu')(cnn_dense)
-main_output = Dense(1)(lstm_out)
-
-model = Model(inputs=[main_input], outputs=[main_output])
-model.compile(loss="mean_squared_error", optimizer='adam')
-
-
 dataset = dp.create_dataset()
 
+# define Inputs
+qdlin_in = Input(shape=(window_size, steps, input_dim), name="Qdlin")
+tdlin_in = Input(shape=(window_size, steps, input_dim), name="Tdlin")
+ir_in = Input(shape=(window_size, input_dim), name="IR")
+
+# combine all data on detail level
+detail_concat = concatenate([qdlin_in, tdlin_in], axis=3, name="detail_concat")
+
+# define CNN
+cnn_out = TimeDistributed(Conv1D(filters=3, kernel_size=3, activation='relu'), name="convolution")(detail_concat)
+cnn_flat = TimeDistributed(Flatten(), name="convolution_flat")(cnn_out)
+
+# combine CNN output with all data on summary level
+all_concat = concatenate([cnn_flat, ir_in], axis=2, name="all_concat")
+
+# define LSTM
+lstm_out = LSTM(20, activation='relu', name="recurrent")(all_concat)
+main_output = Dense(1, name="output")(lstm_out)
+
+model = Model(inputs=[qdlin_in, tdlin_in, ir_in], outputs=[main_output])
+model.compile(loss="mean_squared_error", optimizer='adam')
+
 history = model.fit(dataset, epochs=1, steps_per_epoch=4)
-print(history)
