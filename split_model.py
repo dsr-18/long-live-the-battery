974076595d98e76d6945d3cc8dadbd1967ddb7ac
moxkari <changwendy@gmail.com>
2019-06-03 17:14:14 +0200

    added task.py to interact with Google Cloud.  put split_model code in a method so it can be used by task.py.

--- split_model.py
+++ split_model.py
@@ -1,37 +1,41 @@
 from tensorflow.keras.layers import concatenate, LSTM, Conv1D, Flatten, TimeDistributed, Input, Dense
 from tensorflow.keras.models import Model
-import tensorflow as tf
-import data_pipeline as dp
 
+# TODO use the args
+def create_keras_model(args):
+  """Trains and evaluates the Keras model.
 
-window_size = 5
-steps = 1000
-input_dim = 1
-dataset = dp.create_dataset()
+  Uses the Keras model defined in model.py and trains on data loaded and
+  preprocessed in data_pipeline.py. Saves the trained model in TensorFlow SavedModel
+  format to the path defined in part by the --job-dir argument.
 
-# define Inputs
-qdlin_in = Input(shape=(window_size, steps, input_dim), name="Qdlin")
-tdlin_in = Input(shape=(window_size, steps, input_dim), name="Tdlin")
-ir_in = Input(shape=(window_size, input_dim), name="IR")
+  Args:
+    args: dictionary of arguments - see get_args() for details
+  """
+  window_size = 5
+  steps = 1000
+  input_dim = 1
 
-# combine all data from detail level
-detail_concat = concatenate([qdlin_in, tdlin_in], axis=3, name="detail_concat")
+  # define Inputs
+  qdlin_in = Input(shape=(window_size, steps, input_dim), name='Qdlin')
+  tdlin_in = Input(shape=(window_size, steps, input_dim), name='Tdlin')
+  ir_in = Input(shape=(window_size, input_dim), name='IR')
 
-# define CNN
-cnn_out = TimeDistributed(Conv1D(filters=3, kernel_size=5, activation='relu'), name="convolution")(detail_concat)
-cnn_flat = TimeDistributed(Flatten(), name="convolution_flat")(cnn_out)
+  # combine all data from detail level
+  detail_concat = concatenate([qdlin_in, tdlin_in], axis=3, name='detail_concat')
 
-# combine CNN output with all data from summary level
-all_concat = concatenate([cnn_flat, ir_in], axis=2, name="all_concat")
+  # define CNN
+  cnn_out = TimeDistributed(Conv1D(filters=3, kernel_size=5, activation='relu'), name='convolution')(detail_concat)
+  cnn_flat = TimeDistributed(Flatten(), name='convolution_flat')(cnn_out)
 
-# define LSTM
-lstm_out = LSTM(20, activation='relu', name="recurrent")(all_concat)
-main_output = Dense(1, name="output")(lstm_out)
+  # combine CNN output with all data from summary level
+  all_concat = concatenate([cnn_flat, ir_in], axis=2, name='all_concat')
 
-model = Model(inputs=[qdlin_in, tdlin_in, ir_in], outputs=[main_output])
-model.compile(loss="mean_squared_error", optimizer='adam')
+  # define LSTM
+  lstm_out = LSTM(20, activation='relu', name='recurrent')(all_concat)
+  main_output = Dense(1, name='output')(lstm_out)
 
-tensorboard_log = tf.keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0,
-                                                 write_graph=True, write_images=True)
+  model = Model(inputs=[qdlin_in, tdlin_in, ir_in], outputs=[main_output])
+  model.compile(loss='mean_squared_error', optimizer='adam')
 
-history = model.fit(dataset, epochs=3, steps_per_epoch=4)
+  return model
