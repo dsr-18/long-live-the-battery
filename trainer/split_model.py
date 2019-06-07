96d09167a7b2b732e29e09a04f2c093f2e5b4d2d
moxkari <changwendy@gmail.com>
2019-06-07 17:51:38 +0200

    added maxpool layer & one more conv1d/maxpool pair to reduce output size of model

--- trainer/split_model.py
+++ trainer/split_model.py
@@ -1,6 +1,6 @@
 import trainer.constants as cst
 
-from tensorflow.keras.layers import concatenate, LSTM, Conv1D, Flatten, TimeDistributed, Input, Dense
+from tensorflow.keras.layers import concatenate, LSTM, Conv1D, Flatten, TimeDistributed, Input, Dense, MaxPooling1D
 from tensorflow.keras.models import Model
 
 
@@ -21,7 +21,11 @@ def create_keras_model(window_size, loss, optimizer):
 
     # define CNN
     cnn_out = TimeDistributed(Conv1D(filters=3, kernel_size=5, activation='relu'), name='convolution')(detail_concat)
-    cnn_flat = TimeDistributed(Flatten(), name='convolution_flat')(cnn_out)
+    # Add some maxpools to reduce output size
+    cnn_maxpool = TimeDistributed(MaxPooling1D(), name='conv_pool')(cnn_out)
+    cnn_out2 = TimeDistributed(Conv1D(filters=3, kernel_size=5, activation='relu'), name='conv2')(cnn_maxpool)
+    cnn_maxpool2 = TimeDistributed(MaxPooling1D(), name='pool2')(cnn_out2)
+    cnn_flat = TimeDistributed(Flatten(), name='convolution_flat')(cnn_maxpool2)
 
     # combine CNN output with all data from summary level
     all_concat = concatenate([cnn_flat, ir_in, dt_in], axis=2, name='all_concat')
