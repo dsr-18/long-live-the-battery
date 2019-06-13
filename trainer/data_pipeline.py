c1a53e22b5fe32835ac1150f076280d0a96f5d47
Adem Frenk <adem.frenk@gmail.com>
2019-06-13 21:05:17 +0200

    Added calculate_and_save_scaling_factors and load_scaling_factors

--- trainer/data_pipeline.py
+++ trainer/data_pipeline.py
@@ -216,6 +216,49 @@ def create_dataset(data_dir, window_size, shift, stride, batch_size,
     return assembled_dataset
 
 
+def calculate_and_save_scaling_factors(data_dict, train_test_split, csv_dir):
+    """Calculates the scaling factors for every feature based on the training set in train_test_split
+    and saves the result in a csv file. The factors are used during writing of the tfrecords files."""
+    
+    print("Calculate scaling factors...")
+    scaling_factors = dict()
+    
+    # Calculating max values for summary features
+    for k in [cst.INTERNAL_RESISTANCE_NAME,
+              cst.QD_NAME,
+              cst.REMAINING_CYCLES_NAME,  # The feature "Current_cycles" will be scaled by the same scaling factor
+              cst.DISCHARGE_TIME_NAME]:
+        # Two max() calls are needed, one for every cell, one over all cells
+        scaling_factors[k] = max([max(cell_v["summary"][k])
+                                  for cell_k, cell_v in data_dict.items()
+                                  if cell_k in train_test_split["train"]])
+    
+    # Calculating max values for detail features
+    for k in [cst.QDLIN_NAME,
+              cst.TDLIN_NAME]:
+        # Two max() calls are needed, one over every cycle array, one over all cycles (all cells included)
+        scaling_factors[k] = max([max(cycle_v[k])
+                                  for cell_k, cell_v in data_dict.items()
+                                  if cell_k in train_test_split["train"]
+                                  for cycle_v in cell_v["cycles"].values()])
+    
+    with open(csv_dir, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=scaling_factors.keys())
+        writer.writeheader()  # Write the field names in the first line of the csv
+        writer.writerow(scaling_factors)  # Write values to the corrent fields
+    print("Saved scaling factors to {}".format(csv_dir))
+    print("Scaling factors: {}".format(scaling_factors))
+    return scaling_factors
+
+
+def load_scaling_factors(csv_dir=cst.SCALING_FACTORS_DIR):
+    """Reads the scaling factors from a csv and returns them as a dict."""
+    with open(csv_dir, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            return {k: float(v) for k, v in row.items()}  # Return only the first found line with numeric values
+
+
 # dev method
 def load_train_test_split():
     """
