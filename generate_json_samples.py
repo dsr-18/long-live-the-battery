97cb1debcf21c1fd9d834a4f444532745c83d95e
Rubbedikatz <bonzoxxl@web.de>
2019-06-28 20:02:34 +0200

    Fixed bug in samples generation, consolidated requirements.txt

--- generate_json_samples.py
+++ generate_json_samples.py
@@ -9,6 +9,11 @@ from server.constants import NUM_SAMPLES, SAMPLES_DIR
 These can be used by the 'load random sample' button as examples on the website.
 """
 
+samples_fullpath = os.path.join('server',SAMPLES_DIR)
+
+if not os.path.exists(samples_fullpath):
+    os.makedirs(samples_fullpath)
+    
 dataset = create_dataset(cst.SECONDARY_TEST_SET,
                          window_size=20,
                          shift=1,
@@ -17,7 +22,7 @@ dataset = create_dataset(cst.SECONDARY_TEST_SET,
 rows = dataset.take(NUM_SAMPLES)
 for i, row in enumerate(rows):
     sample = {key: str(value.numpy().tolist()) for key, value in row[0].items()}
-    with open(os.path.join('server',SAMPLES_DIR,'sample_input_{}.json'.format(i+1)), 'w') as outfile:
+    with open(os.path.join(samples_fullpath, 'sample_input_{}.json'.format(i+1)), 'w') as outfile:
         json.dump(sample, outfile)
 print("Created {} sample files in server/static/samples".format(NUM_SAMPLES))
     
